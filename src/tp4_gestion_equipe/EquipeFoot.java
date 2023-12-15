02e584ae090eb231e21c03755708254e9f91f1f7
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2023-12-16 00:23:21 +0100

    polymorphisme

--- src/tp4_gestion_equipe/EquipeFoot.java
+++ src/tp4_gestion_equipe/EquipeFoot.java
@@ -38,4 +38,43 @@ public class EquipeFoot {
 		return this.listeJoueurs;
 	}
 	
+	public String toString() {
+		String ch = "Equipe[ Nom = "+nom+"lisate des joueurs est :\n";
+		for(Personne x : this.listeJoueurs) {
+			ch = ch+"\t-"+x.toString()+"\n";
+		}
+		return ch;
+	}
+	
+	public void ajouterJoueur(Personne x) {
+		if(!this.listeJoueurs.contains(x)) {
+			this.listeJoueurs.add(x);
+		}
+	}
+	
+	public void supprimerJoueur(Personne x) {
+		if(!this.listeJoueurs.contains(x)) {
+			this.listeJoueurs.remove(x);
+		}
+	}
+	
+	public void afficherEtudiants() {
+		System.out.println("Equipe"+this.nom+"cintien les etudiants suivants : \n");
+		for(Personne x : this.listeJoueurs) {
+			 if(x instanceof Etudiant) {
+				 System.out.println("\t- "+x+"\n");
+			 }
+		}
+	}
+	
+	public void afficherProfessuers() {
+		System.out.println("Equipe"+this.nom+"contient les profs suivants : \n");
+		for(Personne x : this.listeJoueurs) {
+			 if(x.getClass().getName().equals("Professeur")) {
+				 System.out.println("\t- "+x+"\n");
+			 }
+		}
+	}
+	
+	
 }
--- src/tp4_gestion_equipe/MainClass.java
+++ src/tp4_gestion_equipe/MainClass.java
@@ -1,10 +1,17 @@
 package tp4_gestion_equipe;
+import java.util.ArrayList;
+import tp3_heritage.Personne;
 
 public class MainClass {
-
+	
+	void testerEquipe(){
+		ArrayList<Personne> x;
+		Personne [] y;
+//		EquipeFoot eq1 = new EquipeFoot("Raja",y);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
